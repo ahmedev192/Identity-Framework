c35c68113b23a4ad1f2ba130cd619589867b895a
Ahmed Mahmoud <ahmedev192@gmail.com>
2024-08-03 12:36:48 +0300

    Passing First And Last Name

--- IdentityFramework/Models/ApplicationUser.cs
+++ IdentityFramework/Models/ApplicationUser.cs
@@ -6,10 +6,12 @@ namespace IdentityFramework.Models
     public class ApplicationUser : IdentityUser
     {
         [Required, MaxLength(50)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Required, MaxLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public byte[] ProfilePicture { get; set; }
+        public byte[]? ProfilePicture { get; set; }
 
 
     }
